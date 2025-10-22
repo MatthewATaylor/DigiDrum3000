17cb57d41a46e47a04f75131a1636f5c1178be7c
charlcoal <charley.reischer@gmail.com>
2025-10-21 23:13:36 -0400

    changed upsampler to use distributed ram instead of a second BRAM18 block

--- hdl/upsampler.sv
+++ hdl/upsampler.sv
@@ -19,22 +19,49 @@ module upsampler (
     output logic [15:0] sample_out  // 16x upsampled, held output
 );
 
-  logic [15:0] sample_buffer  [63:0];
-  logic [ 5:0] buffer_start;
-  logic [ 5:0] sample_index;
-  logic [ 3:0] upsample_index;
-  logic [ 9:0] filter_index;
-  logic [ 7:0] upsample_timer;
+  //logic [15:0] sample_buffer  [63:0];
+  logic [15:0] sample_buffer_out;
+  logic [15:0] sample_buffer_in;
+  logic sample_buffer_we;
+  logic [5:0] sample_buffer_addr;
+
+  dist_ram sample_buffer_lutram (
+      .clk (clk),
+      .addr(sample_buffer_addr),
+      .we  (sample_buffer_we),
+      .din (sample_buffer_in),
+      .dout(sample_buffer_out)
+  );
+
+  logic [5:0] buffer_start;
+  logic [5:0] sample_index;
+  logic [3:0] upsample_index;
+  logic [9:0] filter_index;
+  logic [7:0] sample_timer;
+
+  assign sample_index = sample_timer[5:0];
   assign filter_index = {sample_index, upsample_index};
 
-  logic signed [15:0] sample_pipelined [1:0];
   logic signed [17:0] filter_data;
   logic signed [47:0] accum;
   logic signed [47:0] accumulator_next;
-  assign accumulator_next = accum + (filter_data * sample_pipelined[1]);
+
+  always_comb begin
+    if (sample_in_valid) begin
+      sample_buffer_we   = 1'b1;
+      sample_buffer_addr = buffer_start - 6'h1;
+      sample_buffer_in   = sample_in;
+    end else begin
+      sample_buffer_we   = 1'b0;
+      sample_buffer_addr = buffer_start + sample_index - 6'd2;
+      sample_buffer_in   = 16'hXXXX;
+    end
+  end
+
+  assign accumulator_next = accum + (filter_data * $signed(sample_buffer_out));
 
   logic next_upsample;
-  assign next_upsample = upsample_timer == 141;
+  assign next_upsample = sample_timer == 141;
   // 2 cycle delay
   xilinx_single_port_ram_read_first #(
       .RAM_WIDTH(18),  // Specify RAM data width
@@ -57,9 +84,9 @@ module upsampler (
 
   logic [15:0] next_sample_out;
   always_comb begin
-    if (accum[47] && !(&accum[46:33])) begin
+    if ($signed(accum[47:32]) < -15'sd1) begin
       next_sample_out = 16'h8000;
-    end else if (!accum[47] && (|accum[46:33])) begin
+    end else if ($signed(accum[47:32]) > 15'sd0) begin
       next_sample_out = 16'h7FFF;
     end else begin
       next_sample_out = accum[32:17];
@@ -69,28 +96,19 @@ module upsampler (
   always_ff @(posedge clk) begin
     if (rst) begin
       sample_out <= 0;
-    end else if (upsample_timer == 66) begin
+    end else if (sample_timer == 66) begin
       sample_out <= next_sample_out;
     end
   end
 
-  logic [5:0] sample_buffer_index;
-  assign sample_buffer_index = buffer_start + sample_index;
-
   always_ff @(posedge clk) begin
     if (rst || sample_in_valid || next_upsample) begin
       accum <= 0;
-      sample_pipelined[1] <= 0;
-      sample_pipelined[0] <= 0;
-      sample_index <= 0;
-      upsample_timer <= 0;
+      sample_timer <= 0;
 
     end else begin
-      accum <= accumulator_next;
-      sample_pipelined[1] <= sample_pipelined[0];
-      sample_pipelined[0] <= sample_buffer[sample_buffer_index];
-      sample_index <= sample_index + 6'h1;
-      upsample_timer <= upsample_timer + 8'h1;
+      accum <= sample_timer < 8'd2 ? accum : accumulator_next;
+      sample_timer <= sample_timer + 8'h1;
     end
 
   end
@@ -103,12 +121,29 @@ module upsampler (
     end else begin
       if (sample_in_valid) begin
         upsample_index <= 0;
-        buffer_start <= buffer_start - 6'h1;
-        sample_buffer[buffer_start-6'h1] <= sample_in;
+        buffer_start   <= buffer_start - 6'h1;
 
       end else if (next_upsample) begin
         upsample_index <= upsample_index + 4'h1;
       end
+    end
+  end
+endmodule
+
+module dist_ram (
+    input wire clk,
+    input wire [5:0] addr,
+    input wire we,
+    input wire [15:0] din,
+    output logic [15:0] dout
+);
+
+  logic [15:0] data[63:0];
+  assign dout = data[addr];
+
+  always_ff @(posedge clk) begin
+    if (we) begin
+      data[addr] <= din;
     end
   end
 endmodule
