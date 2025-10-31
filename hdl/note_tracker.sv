c9dbc5c7cbb452a49502bf8f9077fd346b073625
charlcoal <charley.reischer@gmail.com>
2025-10-31 11:37:11 -0400

    initial implimentation of note_tracker and video_sig_gen_basic

---
+++ hdl/note_tracker.sv
@@ -0,0 +1,106 @@
+`timescale 1ns / 1ps  //
+`default_nettype none
+
+module note_tracker #(
+    parameter INSTRUMENT_COUNT = 3,
+    parameter [6:0] MIDI_KEYS[INSTRUMENT_COUNT-1] = {
+      7'd36,  // bd
+      7'd38,  // sd
+      7'd46  // hh_opened
+    }
+) (
+    input wire clk,
+    input wire clk_pixel,
+    input wire rst,
+
+    // on clk
+    input wire midi_valid,
+    input wire [6:0] midi_note,
+    input wire [6:0] midi_velocity,
+
+    // on clk_pixel
+    input wire new_frame,
+    output logic [6:0] inst_velocity[INSTRUMENT_COUNT-1:0]
+);
+
+  localparam [6:0] velocity_decay[INSTRUMENT_COUNT-1:0] = {
+    7'h7F,  // bd
+    7'h10,  // sd
+    7'h03  // hh_opened
+  };
+
+  logic [6:0] midi_note_clk_cross_pipe    [3:0];
+  logic [6:0] midi_velocity_clk_cross_pipe[3:0];
+  // sum encoded, i.e. switches when midi_valid is true
+  logic       midi_valid_clk_cross_pipe   [3:0];
+
+  // on pixel_clk
+  logic       last_midi_valid;
+
+  logic       midi_sig_valid_pixel;
+  assign midi_sig_valid_pixel =
+    midi_note_clk_cross_pipe[3] == midi_note_clk_cross_pipe[2] &&
+    midi_velocity_clk_cross_pipe[3] == midi_velocity_clk_cross_pipe[2] &&
+    midi_valid_clk_cross_pipe[3] != last_midi_valid && !new_frame;
+
+  always_ff @(posedge clk_pixel) begin
+    if (rst) begin
+      last_midi_valid <= 0;
+      integer i;
+      for (i = 0; i < INSTRUMENT_COUNT; i = i + 1) begin
+        inst_velocity[i] <= 0;
+      end
+    end else if (new_frame) begin
+      integer i;
+      for (i = 0; i < INSTRUMENT_COUNT; i = i + 1) begin
+        inst_velocity[i] <= (inst_velocity[i] > velocity_decay[i]) ? inst_velocity[i]-velocity_decay[i] : 0;
+      end
+    end else if (midi_sig_valid_pixel) begin
+      integer i;
+      for (i = 0; i < INSTRUMENT_COUNT; i = i + 1) begin
+        inst_velocity[i] <= midi_note_clk_cross_pipe[3] == MIDI_KEYS[i]
+          ? midi_velocity_clk_cross_pipe[3]
+          : inst_velocity[i];
+      end
+    end
+  end
+
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      midi_valid_clk_cross_pipe[0] <= 0;
+      midi_note_clk_cross_pipe[0] <= 0;
+      midi_velocity_clk_cross_pipe[0] <= 0;
+    end else begin
+      midi_valid_clk_cross_pipe[0] <= midi_valid_clk_cross_pipe[0] ^ midi_valid;
+      midi_note_clk_cross_pipe[0] <= midi_valid ? midi_node : midi_note_clk_cross_pipe[0];
+      midi_velocity_clk_cross_pipe[0] <= midi_valid ? midi_velocity : midi_velocity_clk_cross_pipe[0];
+    end
+  end
+
+  always_ff @(posedge clk_pixel) begin
+    if (rst) begin
+      midi_valid_clk_cross_pipe[1]    <= 0;
+      midi_valid_clk_cross_pipe[2]    <= 0;
+      midi_valid_clk_cross_pipe[3]    <= 0;
+      midi_note_clk_cross_pipe[1]     <= 0;
+      midi_note_clk_cross_pipe[2]     <= 0;
+      midi_note_clk_cross_pipe[3]     <= 0;
+      midi_velocity_clk_cross_pipe[1] <= 0;
+      midi_velocity_clk_cross_pipe[2] <= 0;
+      midi_velocity_clk_cross_pipe[3] <= 0;
+
+    end else begin
+      midi_valid_clk_cross_pipe[1]    <= midi_valid_clk_cross_pipe[0];
+      midi_valid_clk_cross_pipe[2]    <= midi_valid_clk_cross_pipe[1];
+      midi_valid_clk_cross_pipe[3]    <= midi_valid_clk_cross_pipe[2];
+      midi_note_clk_cross_pipe[1]     <= midi_note_clk_cross_pipe[0];
+      midi_note_clk_cross_pipe[2]     <= midi_note_clk_cross_pipe[1];
+      midi_note_clk_cross_pipe[3]     <= midi_note_clk_cross_pipe[2];
+      midi_velocity_clk_cross_pipe[1] <= midi_velocity_clk_cross_pipe[0];
+      midi_velocity_clk_cross_pipe[2] <= midi_velocity_clk_cross_pipe[1];
+      midi_velocity_clk_cross_pipe[3] <= midi_velocity_clk_cross_pipe[2];
+    end
+  end
+endmodule
+
+`default_nettype wire
---
+++ hdl/video_sig_gen_basic.sv
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps  //
+`default_nettype none
+
+module video_sig_gen_basic #(
+    parameter ACTIVE_H_PIXELS = 1280,
+    parameter H_FRONT_PORCH   = 110,
+    parameter H_SYNC_WIDTH    = 40,
+    parameter H_BACK_PORCH    = 220,
+    parameter ACTIVE_LINES    = 720,
+    parameter V_FRONT_PORCH   = 5,
+    parameter V_SYNC_WIDTH    = 5,
+    parameter V_BACK_PORCH    = 20,
+    parameter FPS             = 60
+) (
+    input  wire                             pixel_clk,
+    input  wire                             rst,
+    output logic [$clog2(TOTAL_PIXELS)-1:0] h_count,
+    output logic [ $clog2(TOTAL_LINES)-1:0] v_count,
+    output logic                            new_frame   //single cycle enable signal
+);
+
+  localparam TOTAL_PIXELS = ACTIVE_H_PIXELS + H_FRONT_PORCH + H_SYNC_WIDTH + H_BACK_PORCH;
+  localparam TOTAL_LINES = ACTIVE_LINES + V_FRONT_PORCH + V_SYNC_WIDTH + V_BACK_PORCH;
+
+  logic [$clog2(TOTAL_PIXELS)-1:0] next_h_count;
+  logic [ $clog2(TOTAL_LINES)-1:0] next_v_count;
+
+  assign active_draw = (v_count < ACTIVE_LINES) && (h_count < ACTIVE_H_PIXELS);
+  assign new_frame   = v_count == (ACTIVE_LINES - 1) && h_count == ACTIVE_H_PIXELS;
+
+  always_comb begin
+    if (h_count == TOTAL_PIXELS - 1) begin
+      next_h_count = 0;
+      next_v_count = (v_count == TOTAL_LINES - 1) ? 0 : v_count + 1;
+    end else begin
+      next_h_count = h_count + 1;
+      next_v_count = v_count;
+    end
+
+    if (rst) begin
+      next_v_count = 0;
+      next_h_count = 0;
+    end
+  end
+
+  always_ff @(posedge pixel_clk) begin
+    h_count <= next_h_count;
+    v_count <= next_v_count;
+  end
+
+endmodule  // video_sig_gen
+
+`default_nettype wire
