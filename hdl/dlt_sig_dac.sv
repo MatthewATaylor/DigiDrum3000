10aab18257f51c71a7ffc9b3ea92eafb834f5f3f
charlcoal <charley.reischer@gmail.com>
2025-10-31 09:58:51 -0400

    finally implimented dither correctly

--- hdl/dlt_sig_dac.sv
+++ hdl/dlt_sig_dac.sv
@@ -38,6 +38,10 @@ module dlt_sig_dac_2nd_order (
     input wire [15:0] current_sample,
     output logic audio_out
 );
+  logic [23:0] noise_source;  // period of 16,777,215 at 100MHz repeats less than 20 times per second
+  logic [23:0] noise;
+  logic [23:0] last_noise;
+  logic [23:0] dither;
   logic [17:0] scaled_input;
   // -8 to 7.9999...
   logic [23:0] a_sum;
@@ -49,13 +53,40 @@ module dlt_sig_dac_2nd_order (
   logic last_out;
   // 3/4 scaled to prevent saturation of summers
   assign scaled_input = $signed(current_sample) + ($signed(current_sample) <<< 1);
+  // scramble pseudo-random noise to reduce apparent correlation
+  assign noise = $signed(
+      {
+        noise_source[19],
+        noise_source[16],
+        noise_source[22],
+        noise_source[18],
+        noise_source[15],
+        noise_source[0],
+        noise_source[8],
+        noise_source[11],
+        noise_source[4],
+        noise_source[9],
+        noise_source[12],
+        noise_source[7],
+        noise_source[2],
+        noise_source[13],
+        noise_source[5],
+        noise_source[14],
+        noise_source[3],
+        noise_source[1],
+        noise_source[6],
+        noise_source[10]
+      }
+  );
+  // basically a high-pass basic comb filter to reduce audible frequencies
+  assign dither = noise - last_noise;
 
   // last_out term accounts for unequal rise/fall times (only ever pulsed on
   // for 1 clock cycle)
   assign audio_out = !last_out && ($signed(b_sum) >= 0);
 
   assign a_error = {~scaled_input[17], scaled_input[16:0]} - (audio_out << 19);
-  assign b_error = a_sum + a_error - (audio_out << 19);
+  assign b_error = a_sum + a_error - (audio_out << 19) + dither;
 
   always_comb begin
     next_a_sum = a_sum + a_error;
@@ -77,11 +108,22 @@ module dlt_sig_dac_2nd_order (
 
   always_ff @(posedge clk) begin
     if (rst) begin
+      noise_source <= 24'hFFFFFF;
       a_sum <= 0;
       b_sum <= 0;
       last_out <= 0;
+      last_noise <= 0;
     end else begin
       last_out <= audio_out;
+      noise_source <= {
+        noise_source[22] ^ noise_source[23],
+        noise_source[21] ^ noise_source[23],
+        noise_source[20:17],
+        noise_source[16] ^ noise_source[23],
+        noise_source[15:0],
+        noise_source[23]
+      };
+      last_noise <= noise;
       a_sum <= next_a_sum;
       b_sum <= next_b_sum;
     end
