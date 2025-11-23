12e899c7fcfa6fcee2f7ab84801a4e0ade90d9ce
Matthew Taylor <matthewalantaylor@outlook.com>
2025-11-23 04:03:56 -0500

    Play with integrators

--- scripts/IIR_design.py
+++ scripts/IIR_design.py
@@ -4,14 +4,12 @@ import math
 import scipy.io
 
 CHUNK = 1024
-# SAMPLE_RATE = 44100
-SAMPLE_RATE = 48000
+SAMPLE_RATE = 44100
 FREQ = 220
 SAMPLES_PER_CYCLE = SAMPLE_RATE / FREQ
-DURATION = 10
+DURATION = 5
 MAX = 2**15
-# AMPLITUDE = int(MAX * 0.05)
-AMPLITUDE = int(MAX * 0.2)
+AMPLITUDE = int(MAX * 0.05)
 HALF_CYCLE = int(SAMPLES_PER_CYCLE/2)
 PI = math.pi
 T = 1 / SAMPLE_RATE
@@ -19,26 +17,31 @@ T = 1 / SAMPLE_RATE
 sq = [-AMPLITUDE] * HALF_CYCLE + [AMPLITUDE] * HALF_CYCLE
 sq *= int(DURATION / (1 / FREQ))
 
-# pa = pyaudio.PyAudio()
-# stream = pa.open(
-#     format=pyaudio.paInt16,
-#     channels=1,
-#     rate=44100,
-#     output=True
-# )
+pa = pyaudio.PyAudio()
+stream = pa.open(
+    format=pyaudio.paInt16,
+    channels=1,
+    rate=SAMPLE_RATE,
+    output=True
+)
 
-sq = np.array(sq, dtype=np.int16)
-scipy.io.wavfile.write('./media/[sq].wav', SAMPLE_RATE, sq)
-# prev_integ_in = 0  # sq[0]
-# sq[0] = 0
-# for i in range(1, len(sq)):
-#     t = i * T
-#     wc = 2 * PI * 10000/DURATION * t
-#     integ_in = (sq[i] - sq[i-1])
-#     sq[i] = sq[i-1] + 0.5 * (integ_in + prev_integ_in) * wc * T
-#     prev_integ_in = integ_in
+x = np.array(sq, dtype=np.int16)
+y = np.zeros(len(x), dtype=np.int16)
+s = 0
+for i in range(0, len(x)):
+    t = i * T
+    wc = 2 * PI * 10000/DURATION * t
 
-# stream.write(sq.tobytes())
-# stream.close()
-# pa.terminate()
+    # Prewarped gain
+    g = math.tan(wc * T / 2)
+
+    # LPF with transposed trapezoidal integrator
+    G = g / (1 + g)
+    v = G * (x[i] - s)
+    y[i] = v + s
+    s = y[i] + v
+
+stream.write(y.tobytes())
+stream.close()
+pa.terminate()
 
