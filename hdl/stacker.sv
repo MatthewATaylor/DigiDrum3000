a7702d6c95688db66aacc5c3fe37f8b3697058fd
charlcoal <charley.reischer@gmail.com>
2025-12-04 22:20:14 -0500

    changed stacker to auto-align tlast signal

--- hdl/stacker.sv
+++ hdl/stacker.sv
@@ -25,12 +25,11 @@ module stacker(
     );
     logic [127:0] data_recent;
     logic [2:0]   count;
-    logic [7:0]   tlast_recent;
 
     logic         accept_in;
     assign accept_in = pixel_tvalid && pixel_tready;
 
-    assign pixel_tready = (count == 7) ? chunk_tready : 1'b1;
+    assign pixel_tready = (count == 7 || pixel_tlast) ? chunk_tready : 1'b1;
 
     logic accept_out;
     assign accept_out = chunk_tready && chunk_tvalid;
@@ -39,18 +38,17 @@ module stacker(
         if(rst) begin
             data_recent  <= 127'b0;
             count        <= 0;
-            tlast_recent <= 8'b0;
             chunk_tvalid <= 1'b0;
-            chunk_tlast  <= 0;
         end else begin
             if (accept_in) begin
                 data_recent  <= { pixel_tdata[15:0], data_recent[127:16] };
-                tlast_recent <= { pixel_tlast, tlast_recent[7:1] };
-                count        <= count + 1;
-                if (count == 7) begin
+                if (count == 7 || pixel_tlast) begin
                     chunk_tdata  <= { pixel_tdata[15:0], data_recent[127:16] };
-                    chunk_tlast <= pixel_tlast || (tlast_recent > 0);
+                    chunk_tlast <= pixel_tlast;
                     chunk_tvalid <= 1'b1;
+                    count <= 0;
+                end else begin
+                    count        <= count + 1;
                 end
             end
             if (accept_out) begin
